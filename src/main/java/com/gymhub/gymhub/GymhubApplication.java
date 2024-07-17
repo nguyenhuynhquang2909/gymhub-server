c41c0bfb7bf27b1b97d9a94ea43e1306267d4959
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-07-17 20:57:53 +0700

    Test

--- src/main/java/com/gymhub/gymhub/GymhubApplication.java
+++ src/main/java/com/gymhub/gymhub/GymhubApplication.java
@@ -2,10 +2,20 @@ package com.gymhub.gymhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class GymhubApplication {
 
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Java";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GymhubApplication.class, args);
 	}
