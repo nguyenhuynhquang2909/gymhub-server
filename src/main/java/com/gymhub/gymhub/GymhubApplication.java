8be5b697a4dc51f4c096d2fada9cd003e484ee02
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-07-22 10:55:04 +0700

    Update cross origin

--- src/main/java/com/gymhub/gymhub/GymhubApplication.java
+++ src/main/java/com/gymhub/gymhub/GymhubApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @SpringBootApplication
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class GymhubApplication {
 
