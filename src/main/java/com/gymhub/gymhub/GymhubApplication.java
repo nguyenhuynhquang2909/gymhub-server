aa61524dfa3c2a5d1dd008bfa8b489d2238567f2
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-07-22 11:02:30 +0700

    Enable cors

--- src/main/java/com/gymhub/gymhub/GymhubApplication.java
+++ src/main/java/com/gymhub/gymhub/GymhubApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @SpringBootApplication
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class GymhubApplication {
 
