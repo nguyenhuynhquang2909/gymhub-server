2d9532fe6b94e1129cb9aa4e511f5d4419a0ebbf
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-07-22 10:24:54 +0700

    Update mapping controller

--- src/main/java/com/gymhub/gymhub/controller/MappingsController.java
+++ src/main/java/com/gymhub/gymhub/controller/MappingsController.java
@@ -2,8 +2,8 @@ package com.gymhub.gymhub.controller;
 
 import java.util.Map;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
