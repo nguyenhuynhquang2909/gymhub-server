0733aa39d7a8c7f85f22f13733919a4490fe2a1b
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-07-22 11:03:22 +0700

    Update

--- src/main/java/com/gymhub/gymhub/config/WebConfig.java
+++ src/main/java/com/gymhub/gymhub/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://14.241.129.58")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true).maxAge(3600);
