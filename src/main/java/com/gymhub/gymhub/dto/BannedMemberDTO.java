f9e6eec6616727c35360570ed8b879aefe1ddc3d
duongtran1004 <S3979381@rmit.edu.vn>
2024-09-05 15:56:21 +0700

    fixed ban memebr

--- src/main/java/com/gymhub/gymhub/dto/BannedMemberDTO.java
+++ src/main/java/com/gymhub/gymhub/dto/BannedMemberDTO.java
@@ -2,6 +2,7 @@ package com.gymhub.gymhub.dto;
 
 import java.util.Date;
 
+
 public class BannedMemberDTO {
 
     private Long id;
@@ -15,4 +16,23 @@ public class BannedMemberDTO {
         this.bannedUntil = bannedUntil;
         this.reason = reason;
     }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Date getBannedUntil() {
+        return bannedUntil;
+    }
+
+    public String getReason() {
+        return reason;
+    }
 }
+
+//objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
